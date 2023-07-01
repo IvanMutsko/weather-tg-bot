4a80df0cc35f575bebe2ef0dfb488f18e257a3bb
Ivan Mutsko <imucko1996@gmail.com>
2023-07-01 23:21:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,8 +19,11 @@ const options = {
     keyboard: [
       [
         {
-          text: "Send Location",
+          text: "Weather in your location",
           request_location: true,
+        },
+        {
+          text: "Weather in another location",
         },
       ],
     ],
@@ -33,30 +36,33 @@ const start = () => {
     const text = msg.text;
     const chatId = msg.chat.id;
 
-    if (text === "/start") {
-      await bot.sendMessage(
-        chatId,
-        `Hello, I'm a little helper who can tell about the weather.`
-      );
-      await bot.sendSticker(
-        chatId,
-        "https://tlgrm.eu/_/stickers/9d4/44b/9d444bb6-d895-3ddc-9d38-44981f03bc65/5.webp"
-      );
+    try {
+      if (text === "/start") {
+        await bot.sendMessage(
+          chatId,
+          `Hello, I'm a little helper who can tell about the weather.`
+        );
+        await bot.sendSticker(
+          chatId,
+          "https://tlgrm.eu/_/stickers/9d4/44b/9d444bb6-d895-3ddc-9d38-44981f03bc65/5.webp"
+        );
 
-      bot.sendMessage(
-        chatId,
-        "Click the button to send your location",
-        options
-      );
-      return;
-    }
+        // an empty line so that an error does not appear
+        await bot.sendMessage(chatId, " ", options);
 
-    if (text === "/help") {
-      bot.sendMessage(
-        chatId,
-        `If you are interested in knowing the current weather, send your location.`
-      );
-      return;
+        return;
+      }
+
+      if (text === "/help") {
+        bot.sendMessage(
+          chatId,
+          `If you are interested in knowing the current weather, send your location.`
+        );
+
+        return;
+      }
+    } catch (error) {
+      console.log(error);
     }
   });
 
@@ -64,24 +70,45 @@ const start = () => {
     const location = msg.location;
     const chatId = msg.chat.id;
 
-    // weather response if will be send location
-    if (location) {
-      const { latitude, longitude } = location;
+    try {
+      // weather response if will be send location
+      if (location) {
+        const { latitude, longitude } = location;
 
-      const response = await fetchWeather(latitude, longitude);
-      // weather message
-      bot.sendMessage(chatId, createMarkupWeather(response));
-      // weather icon
-      await bot.sendPhoto(
-        chatId,
-        `https://openweathermap.org/img/wn/${response.icon}@2x.png`
-      );
+        const response = await fetchWeather(latitude, longitude);
+        // weather message
+        await bot.sendMessage(chatId, createMarkupWeather(response));
+        // weather icon
+        await bot.sendPhoto(
+          chatId,
+          `https://openweathermap.org/img/wn/${response.icon}@2x.png`
+        );
+      }
+    } catch (error) {
+      console.log(error);
     }
   });
+  // =====================================
+
+  // catch text from chat
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+
+    console.log(chatId);
+
+    
+  });
+  // =====================================
+
+  // catch text from chat
+  // bot.on("message", async (query) => {
+  // const chatId = query.message.chat.id;
+  // const buttonId = query.data;
+
+  // console.log(query);
+  // });
+
+  // ======================================
 };
 
 start();
-
-// Enable graceful stop
-// process.once("SIGINT", () => bot.stop("SIGINT"));
-// process.once("SIGTERM", () => bot.stop("SIGTERM"));
